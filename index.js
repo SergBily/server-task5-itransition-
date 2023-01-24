c53f7736067f5faa6fe737fa488001093573e0cd
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-24 10:00:13 +0300

    init: start server for task5

---
+++ index.js
@@ -0,0 +1,38 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import { router } from './routes/routes.js';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+app.use(cors({
+  origin: [process.env.CLIENT_URL],
+  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
+  credentials: true,
+}));
+app.use('/', router);
+
+const start = () => {
+  mongoose.set('strictQuery', false);
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Base work');
+  });
+  app.listen(PORT, () => {
+    console.log('Port work');
+  });
+};
+
+start();
