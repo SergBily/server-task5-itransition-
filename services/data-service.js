267a64a2e881056bcd7d2c88a7a16ddf56652f4a
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-25 19:53:39 +0300

    fix: data service

--- services/data-service.js
+++ services/data-service.js
@@ -1,23 +1,49 @@
 import { faker } from '@faker-js/faker';
+import seedrandom from 'seedrandom';
 import { getPhoneSnippet } from '../utils/phone.js';
+import { generateErrors } from './errors-service.js';
 
 class DataService {
-  fakerUsers = [];
+  fakesData = [];
 
-  async getfakesData(region, seed) {
-    this.fakerUsers = [];
-    const usersOnPage = 10;
-    for (let i = 0; i < usersOnPage; i += 1) {
-      const createdUser = DataService.createRandomData(region);
-      createdUser.number = String(i + 1);
-      this.fakerUsers.push(createdUser);
+  currentSeed = '';
+
+  dataOnPage = 10;
+
+  generatedNumbers = [];
+
+  countNumberData = 1;
+
+  getfakesData(region, seed, page, errorRate) {
+    const userSeed = seed + page;
+    this.updateSeed(page, seed);
+    this.cryptoSeed(userSeed);
+    let newData = [];
+    for (let i = 0; i < this.dataOnPage; i += 1) {
+      const createdData = DataService.createRandomData(region, this.generatedNumbers[i]);
+      newData.push(createdData);
     }
-    return this.fakerUsers;
+    if (errorRate) {
+      newData = generateErrors.getDataWithErrors(newData, region, errorRate, userSeed);
+      this.addNumberToData(newData);
+    } else {
+      this.addNumberToData(newData);
+    }
+    return this.fakesData;
   }
 
-  static createRandomData(region) {
+  addNumberToData(d) {
+    const b = d;
+    for (let i = 0; i < b.length; i += 1) {
+      b[i].number = this.countNumberData;
+      this.fakesData.push(b[i]);
+      this.countNumberData += 1;
+    }
+  }
+
+  static createRandomData(region, seed) {
     faker.locale = region;
-    // faker.seed(i);
+    faker.seed(seed);
     const sex = faker.name.sexType();
     const firstName = faker.name.firstName(sex);
     const lastName = faker.name.lastName(sex);
@@ -25,16 +51,37 @@ class DataService {
     return {
       _id: faker.datatype.uuid(),
       fullName: `${firstName} ${lastName}`,
-      address: `
-      ${faker.helpers.arrayElement([faker.address.city(), faker.address.cityName()])}
-       ${faker.helpers.arrayElement([faker.address.state(), faker.address.stateAbbr()])}
-        ${faker.helpers.arrayElement([faker.address.streetName(), faker.address.streetAddress(false), faker.address.streetAddress()])}
-         ${faker.address.buildingNumber()}
-          ${faker.address.zipCode()}`.trim(),
+      address:
+        `${faker.address.zipCode()
+        }${faker.address.buildingNumber()
+        } ${faker.helpers.arrayElement([faker.address.streetAddress(false),
+          faker.address.streetAddress()])
+        } ${faker.helpers.arrayElement([faker.address.city(), faker.address.cityName()])
+        } ${faker.helpers.arrayElement([faker.address.state(), faker.address.stateAbbr()])
+        }`.trim(),
       phone: faker.helpers.arrayElement(
         [faker.phone.number(phoneSnippet[0]), faker.phone.number(phoneSnippet[1])],
       ),
     };
+  }
+
+  updateSeed(p, s) {
+    if (p === 1) {
+      this.currentSeed = s;
+      this.fakesData = [];
+      this.dataOnPage = 20;
+      this.countNumberData = 1;
+    } else {
+      this.dataOnPage = 10;
+    }
+  }
+
+  cryptoSeed(s) {
+    this.generatedNumbers = [];
+    const rng = seedrandom(s);
+    for (let i = 0; i < this.dataOnPage; i += 1) {
+      this.generatedNumbers.push(rng.int32());
+    }
   }
 }
 
