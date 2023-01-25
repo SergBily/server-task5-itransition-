cdf9cdf20a96dce28379c00f5dae6dcaf7ea0afc
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-25 19:48:14 +0300

    fix: export to csv

--- controllers/csv-controller.js
+++ controllers/csv-controller.js
@@ -1,10 +1,11 @@
+// import fs from 'fs/promises';
 import { csvService } from '../services/csv-service.js';
 
 class CsvController {
   async exportToCsv(request, response, next) {
     try {
       const csvData = csvService.exportToCsv();
-      response.setHeader('Content-Type', 'blob');
+      response.setHeader('Content-Type', 'text/csv');
       response.setHeader('Content-Disposition', 'attachment; filename=fakes_data.csv');
       return response.status(200).end(csvData);
     } catch (e) {
