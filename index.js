dee5a913fdaf6102887af263564a6f3f213e0f3d
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-25 21:48:34 +0300

    refactor: add headers cors

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(cors({
