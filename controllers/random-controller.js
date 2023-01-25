113a40515dc9b918279999f692369b24e515cc15
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-25 19:49:31 +0300

    fix: generate fake data

--- controllers/random-controller.js
+++ controllers/random-controller.js
@@ -1,11 +1,12 @@
 import { dataService } from '../services/data-service.js';
 
 class RandomController {
-  async getFakerData(request, response, next) {
+  getFakesData(request, response, next) {
     try {
-      const { seed, errorRate, region } = request.body;
-      const fakerData = await dataService.getfakesData(region, seed);
-      console.log(fakerData);
+      const {
+        seed, errorRate, region, page,
+      } = request.body;
+      const fakerData = dataService.getfakesData(region, seed, page, errorRate);
       return response.json(fakerData);
     } catch (e) {
       next(e);
