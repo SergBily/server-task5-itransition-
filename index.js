ceeda09b52632d98ab523eef5edf597d9940be5f
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-26 06:28:19 +0300

    fix: cors

--- index.js
+++ index.js
@@ -8,29 +8,27 @@ import { router } from './routes/routes.js';
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://task5-itransition.netlify.app');
-  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 app.use(cors({
   origin: [process.env.CLIENT_URL],
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   credentials: true,
 }));
 app.use('/', router);
 
 const start = () => {
-  mongoose.set('strictQuery', false);
   mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  app.listen(PORT, () => {
-    console.log(process.env.CLIENT_URL);
-  });
+  app.listen(PORT);
 };
 
 start();
