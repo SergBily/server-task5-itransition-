e6c8a9c0311986473fe5207a6d93464a0869e89f
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-26 06:47:14 +0300

    fix: add headers

--- index.js
+++ index.js
@@ -12,15 +12,16 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+  );
   next();
 });
-app.use(cors({
-  origin: [process.env.CLIENT_URL],
-  methods: ['GET', 'POST', 'DELETE', 'PATCH'],
-  credentials: true,
-}));
+app.use(cors());
 app.use('/', router);
 
 const start = () => {
