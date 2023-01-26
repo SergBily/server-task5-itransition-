2995d95d2047aa6cbdec99d97599c62107ad47b7
SergBily <89011743+SergBily@users.noreply.github.com>
2023-01-26 08:10:09 +0300

    fix: headers cors

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use((req, res, next) => {
@@ -19,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.use(cors({
-  origin: '*',
+  origin: [process.env.CLIENT_URL],
   methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   credentials: true,
 }));
